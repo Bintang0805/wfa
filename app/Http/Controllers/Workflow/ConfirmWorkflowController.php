587d2b021359e79e1794a0a2d5dce8da8c1fe422
Bintang0805 <ikhsanbintang3292@gmail.com>
2023-07-13 21:14:46 +0700

    Fix Revision

--- app/Http/Controllers/Workflow/ConfirmWorkflowController.php
+++ app/Http/Controllers/Workflow/ConfirmWorkflowController.php
@@ -11,44 +11,64 @@ use Illuminate\Support\Facades\Auth;
 
 class ConfirmWorkflowController extends Controller
 {
-    public function index () {
-      // dd(ApprovedWorkflow::with(["request_workflow.workflow", "request_workflow.user"])->where("role_id", Auth::user()->id)->get());
+  public function index()
+  {
+    // dd(ApprovedWorkflow::with(["request_workflow.workflow", "request_workflow.user"])->where("role_id", Auth::user()->id)->get());
 
-      // $workflow = $workflow->where("id", 2)->get();
-      $workflowApprovers = ApprovedWorkflow::with(["request_workflow.workflow", "request_workflow.user"])->where("role_id", Auth::user()->id)->where("approved", null)->get();
+    // $workflow = $workflow->where("id", 2)->get();
+    $workflowApprovers = ApprovedWorkflow::with(["request_workflow.workflow", "request_workflow.user"])->where("approved", null)->get()->unique("request_workflow_id");
 
-      // dd($workflowApprovers);
-      // $workflow = RequestWorkflow::whereStatus("pending");
+    // dd($workflowApprovers);
+    $workflowApprovers = $workflowApprovers->where("role_id", Auth::user()->role->id);
+    // $workflow = RequestWorkflow::whereStatus("pending");
 
-      // foreach ($workflowApprovers as $workflowApprover) {
-      //   $workflow = $workflow->orWhere("workflow_id", $workflowApprover->workflow_id);
-      // }
+    // dd($workflowApprovers);
+    $newWorkflowApprovers = [];
+    foreach ($workflowApprovers as $workflowApprover) {
+      $getNewWorkflowApprover = ApprovedWorkflow::with(["request_workflow.workflow", "request_workflow.user"])->where("request_workflow_id", $workflowApprover->request_workflow_id)->where("approved", "!=", 0)->first();
 
-      // $workflow = $workflow->with(["workflow", "user"])->get();
-
-      // // $requestWorkflow = $workflow->where("approver_roles", Auth::user()->id)->get();
-      // dd($workflow);
-      $data = [
-        "workflow_approvers" => $workflowApprovers,
-      ];
-
-      return view("workflow.confirm-workflow.index", $data);
-    }
-
-    public function confirm(Request $request) {
-      $ApprovedWorkflow = ApprovedWorkflow::where("id", $request->approved_workflow_id)->first();
-
-      if($request->button == 1) {
-        $confirmWorkflow = [
-          "approved" => true,
-        ];
-      } else {
-        $confirmWorkflow = [
-          "approved" => false,
-        ];
+      // dump($getRejectedData);
+      if($getNewWorkflowApprover) {
+        array_push($newWorkflowApprovers, $getNewWorkflowApprover);
       }
-      $ApprovedWorkflow->update($confirmWorkflow);
-
-      return redirect()->route("confirm-workflows.index");
     }
+
+    if(count($newWorkflowApprovers) != 0) {
+      $workflowApprovers = $newWorkflowApprovers;
+    }
+
+
+    // dd($workflowApprovers);
+    // foreach ($workflowApprovers as $workflowApprover) {
+    //   $workflow = $workflow->orWhere("workflow_id", $workflowApprover->workflow_id);
+    // }
+
+    // $workflow = $workflow->with(["workflow", "user"])->get();
+
+    // // $requestWorkflow = $workflow->where("approver_roles", Auth::user()->id)->get();
+    // dd($workflow);
+    $data = [
+      "workflow_approvers" => $workflowApprovers,
+    ];
+
+    return view("workflow.confirm-workflow.index", $data);
+  }
+
+  public function confirm(Request $request)
+  {
+    $ApprovedWorkflow = ApprovedWorkflow::where("id", $request->approved_workflow_id)->first();
+
+    if ($request->button == 1) {
+      $confirmWorkflow = [
+        "approved" => true,
+      ];
+    } else {
+      $confirmWorkflow = [
+        "approved" => false,
+      ];
+    }
+    $ApprovedWorkflow->update($confirmWorkflow);
+
+    return redirect()->route("confirm-workflows.index");
+  }
 }
